4d058542513dbb542c503e1e9afb1cd3cbe36cd4
gale <g4lthree@gmail.com>
2022-03-14 21:56:23 +0700

    tambah contact

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,13 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
+          <a href="http://localhost:3000/about" className={styles.card}>
+            <h2>About&rarr;</h2>
+            <p>Sembarang wes sak karepmu</p>
+          </a>
+        </div>
+
+        <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
